4d62e6736abaf0fb493324819de3db167712d7cf
MdShuvo18 <mdks33881@gmail.com>
2024-08-16 23:31:49 +0600

    pagination primary added

--- src/Pages/AllProducts.jsx
+++ src/Pages/AllProducts.jsx
@@ -25,30 +25,42 @@ const AllProducts = () => {
     console.log(typeof (items.count))
     const itemsPerPage = 10;
     const totalPages = Math.ceil(items.count / itemsPerPage);
-    // console.log(totalPages)
-    // const pages = [...Array(totalPages).keys()]
-    // console.log(pages)
     const pages = []
     for (let i = 0; i < totalPages; i++) {
         pages.push(i + 1)
     }
-    console.log(pages)
+    // console.log(pages)
     return (
-        <div className="grid grid-cols-3 gap-5 justify-items-center">
-            {products.map((product) => (
-                <div key={product._id} className="card card-compact bg-base-100 w-96 shadow-xl">
-                    <figure>
-                        <img src={product.productImage} alt="" />
-                    </figure>
-                    <div className="card-body">
-                        <h2 className="card-title">{product.productName}</h2>
-                        <p>{product.description}</p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
+        <div>
+            <div className="grid grid-cols-3 gap-5 justify-items-center">
+                {products.map((product) => (
+                    <div key={product._id} className="card card-compact bg-base-100 w-96 shadow-xl">
+                        <figure>
+                            <img src={product.productImage} alt="" />
+                        </figure>
+                        <div className="card-body">
+                            <h2 className="card-title">{product.productName}</h2>
+                            <p>{product.description}</p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary">Buy Now</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
+            <div className="text-center">
+                {
+                    pages.map(page => <div key={page} className="join gap-2">
+                        <input
+                            className="join-item btn btn-square"
+                            type="radio"
+                            name="options"
+                            aria-label={page}
+                        />
+
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
